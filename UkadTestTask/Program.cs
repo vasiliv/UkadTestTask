bc11b81f0f7d1ddd86bd09e80cd5d72b3f01ff11
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-19 18:08:21 +0400

    v3  finished

--- UkadTestTask/Program.cs
+++ UkadTestTask/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Xml;
 
 namespace UkadTestTask
@@ -15,19 +17,44 @@ namespace UkadTestTask
             Stopwatch stopwatch = new Stopwatch();
             List<Url> urlList = new List<Url>();
 
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load("https://www.ambebi.ge/");
+
+            //Use the SelectNodes method to find all the "a" elements in the document:
+            HtmlNodeCollection htmlNodes = htmlDoc.DocumentNode.SelectNodes("//a");
+
+            //Iterate through the nodes and check the "href" attribute of each node to see if it starts with "http"
+            foreach (HtmlNode node in htmlNodes)
+            {
+                stopwatch.Start();
+                if (node.Attributes["href"].Value.StartsWith("http"))
+                {
+                    stopwatch.Stop();
+                    TimeSpan ts = stopwatch.Elapsed;
+                    urlList.Add(new Url { UrlName = node.Attributes["href"].Value, ElapsedTime = ts.ToString() });
+                }
+            }
+            Console.WriteLine($"Urls(html documents) found after crawling a website: {urlList.Count}");
+
             //Sitemap.xml file is placed at \UkadTestTask\UkadTestTask\bin\Debug\net5.0 folder
-            XmlDocument doc = new XmlDocument();
-            doc.Load("Sitemap.xml");            
-            XmlNodeList nodes = doc.SelectNodes("/root/url");
-            foreach (XmlNode node in nodes)
+            XmlDocument xmlDoc = new XmlDocument();
+            List<Url> xmlList = new List<Url>();
+            xmlDoc.Load("Sitemap.xml");            
+            XmlNodeList xmlNodes = xmlDoc.SelectNodes("/root/url");
+            foreach (XmlNode node in xmlNodes)
             {
                 stopwatch.Start();                
-                urlList.Add(new Url { UrlName = node.InnerText });
+                xmlList.Add(new Url { UrlName = node.InnerText });
                 stopwatch.Stop();
                 TimeSpan ts = stopwatch.Elapsed;
-                urlList.Add(new Url { ElapsedTime = ts.ToString() });                                
+                xmlList.Add(new Url { ElapsedTime = ts.ToString() });                                
             }
-            foreach (var item in urlList)
+            Console.WriteLine($"Urls found in sitemap: {xmlList.Count}");
+
+            //Concatinate url and xml Lists
+            List<Url> totalList = urlList.Concat(xmlList).ToList();
+
+            foreach (var item in totalList)
             {
                 Console.WriteLine($"{item.UrlName} {item.ElapsedTime}");
             }
