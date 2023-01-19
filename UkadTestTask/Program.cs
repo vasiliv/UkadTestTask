3069a3c4b58c4e58f0797c28db7354dff6098991
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-19 11:27:35 +0400

    List<Url> added

--- UkadTestTask/Program.cs
+++ UkadTestTask/Program.cs
@@ -11,21 +11,26 @@ namespace UkadTestTask
         {
             //Console.Write("Please enter url address: ");
             //string urlName = Console.ReadLine();
-
-            Dictionary <string,double> xmlDict = new Dictionary<string,double>();
+            
             Stopwatch stopwatch = new Stopwatch();
+            List<Url> urlList = new List<Url>();
+
             //Sitemap.xml file is placed at \UkadTestTask\UkadTestTask\bin\Debug\net5.0 folder
             XmlDocument doc = new XmlDocument();
-            doc.Load("Sitemap.xml");
-            //Console.WriteLine(doc.InnerXml);
+            doc.Load("Sitemap.xml");            
             XmlNodeList nodes = doc.SelectNodes("/root/url");
             foreach (XmlNode node in nodes)
             {
-                stopwatch.Start();
-                xmlDict.Add(node.InnerText, stopwatch.ElapsedMilliseconds);
+                stopwatch.Start();                
+                urlList.Add(new Url { UrlName = node.InnerText });
                 stopwatch.Stop();
-                Console.WriteLine($"{node.InnerText} {stopwatch.ElapsedMilliseconds}");                
-            }            
+                TimeSpan ts = stopwatch.Elapsed;
+                urlList.Add(new Url { ElapsedTime = ts.ToString() });                                
+            }
+            foreach (var item in urlList)
+            {
+                Console.WriteLine($"{item.UrlName} {item.ElapsedTime}");
+            }
         }
     }
 }
---
+++ UkadTestTask/Url.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkadTestTask
+{
+    public class Url
+    {
+        public string UrlName { get; set; }
+        public string ElapsedTime { get; set; }
+    }
+}
