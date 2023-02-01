59dcadfeee1b2128dfcb267987af848208883d29
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-02-01 12:40:05 +0400

    old time, eliminated outer links, eliminiated list duplication

--- UkadTestTask/Program.cs
+++ UkadTestTask/Program.cs
@@ -2,38 +2,49 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Xml;
 using UkadTestTask;
 
+Stopwatch stopwatch = new Stopwatch();
 List<Url> urlList = new List<Url>();
 List<Url> xmlList = new List<Url>();
 
-//Console.Write("Please enter url address without / at the end: ");
-//string urlName = Console.ReadLine();
+Console.Write("Please enter url address: ");
+string urlName = Console.ReadLine().TrimEnd('/');
 
-string urlName = "https://jwt.io";
+//string urlName = "https://jwt.io/";
 //string urlName = "https://lenta.ru";
+//string urlName = "https://www.google.com/";
+//string urlName = "https://ss.ge";
+
+//string filePath = @"c:\Projects\Ukad.txt";
 
 HtmlWeb web = new HtmlWeb();
 if (Helper.UrlExists(urlName))
 {
     HtmlDocument htmlDoc = web.Load(urlName);
+    //File.WriteAllText(filePath, htmlDoc.Text);
 
     //Use the SelectNodes method to find all the "a" elements in the document:
     HtmlNodeCollection htmlNodes = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
 
     foreach (HtmlNode node in htmlNodes)
     {
-        if (node.OuterHtml.Contains("/"))
+        stopwatch.Start();
+        //Select only outer links
+        if (node.Attributes["href"].Value.Contains("http"))
         {
-            urlList.Add(new Url { UrlName = node.Attributes["href"].Value.TrimEnd('/'), ElapsedTime = Helper.GetResponseTime(urlName) });
+            stopwatch.Stop();
+            urlList.Add(new Url { UrlName = node.Attributes["href"].Value.TrimEnd('/'), ElapsedTime = stopwatch.ElapsedMilliseconds });
         }        
     }
 }
 else
 {
     Console.WriteLine("Url does not exist");
+    return;
 }
 
 XmlDocument xmlDoc = new XmlDocument();
@@ -45,8 +56,14 @@ if (Helper.UrlExists(sitemapUrl))
 
     foreach (XmlNode node in xmlDoc.DocumentElement)
     {
-        //add urls located in <loc> node to xmlList
-        xmlList.Add(new Url { UrlName = node.FirstChild.InnerXml, ElapsedTime = Helper.GetResponseTime(sitemapUrl) });
+        stopwatch.Start();
+        //Select only outer links
+        if (node.InnerText.Contains("http"))
+        {
+            stopwatch.Stop();
+            //add urls located in <loc> node to xmlList
+            xmlList.Add(new Url { UrlName = node.FirstChild.InnerXml, ElapsedTime = stopwatch.ElapsedMilliseconds });
+        }
     }
 }
 else
@@ -54,24 +71,25 @@ else
     Console.WriteLine("Sitemap.xml does not exist");
 }
 
+//Extract only unique members
+urlList = urlList.GroupBy(u => u.UrlName).Select(u => u.FirstOrDefault()).ToList();
 Console.WriteLine($"Urls(html documents) found after crawling a website: {urlList.Count}");
 Console.WriteLine($"Urls found in sitemap.xml: {xmlList.Count}");
 
 var xmlListOnlyName = xmlList.Select(x => x.UrlName);
 var urlListOnlyName = urlList.Select(x => x.UrlName);
 
-Console.WriteLine();
-Console.WriteLine("1. Merge ordered by timing");
+Console.WriteLine("\n1. Merge ordered by timing");
 var totalList = xmlList.Concat(urlList).ToList();
 Helper.PrintOrderedByTime(totalList);
 
 Console.WriteLine();
-Console.WriteLine("2. Urls founded in sitemap.xml but not founded after crawling a web site:");
+Console.WriteLine("\n2. Urls founded in sitemap.xml but not founded after crawling a web site:");
 var exceptXml = xmlListOnlyName.Except(urlListOnlyName).ToList();
 Helper.Print(exceptXml);
 
 Console.WriteLine();
-Console.WriteLine("3. Urls founded by the crawling the website but not in sitemap.xml");
+Console.WriteLine("\n3. Urls founded by the crawling the website but not in sitemap.xml");
 var exceptUrl = urlListOnlyName.Except(xmlListOnlyName).ToList();
 Helper.Print(exceptUrl);
 
